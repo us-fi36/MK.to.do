e5e5e59076f41bb51fcb59c89c7d4fbfc07ab275
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-27 14:34:24 +0200

    Create todoapi.php

---
+++ todoapi.php
@@ -0,0 +1,16 @@
+<?php
+
+header ("Content-Type: aplication/json");
+ switch ($_Server[request_Method]){
+  case "GET":
+      BREAK;
+    //get to do's (read)
+  case "POST":
+      BREAK;
+  Case "PUT":
+      BREAK;
+  Case "DELETE": 
+      BREAK;
+}
+
+?>
