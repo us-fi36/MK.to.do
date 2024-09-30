b52f080b57108c4390c3a7fce83c2d45bc1232d4
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-30 13:54:57 +0200

    Delete get.list.php

--- get.list.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Read content of the file and decode JSON data to an array.
-$todo_file = 'todos.json';
-$todo_items = json_decode(file_get_contents($todo_file), true);
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-// Rückgabe der TODOs
-echo json_encode($todo_items);
-}
-?>
