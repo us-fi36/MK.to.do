5dc38177d051ca31bdfa8b0c9607a722276cb4f3
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-30 10:11:38 +0200

    Update todoapi.php
    changed overall line and expression

--- todoapi.php
+++ todoapi.php
@@ -1,16 +1,17 @@
-<?php
-
-header ("Content-Type: aplication/json");
- switch ($_Server[request_Method]){
-  case "GET":
-      BREAK;
-    //get to do's (read)
-  case "POST":
-      BREAK;
-  Case "PUT":
-      BREAK;
-  Case "DELETE": 
-      BREAK;
+<<?php
+header('Content-Type: application/json');
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'GET':
+// Placeholder for reading TODO items
+break;
+case 'POST':
+// Placeholder for creating a new TODO
+break;
+case 'PUT':
+// Placeholder for updating a TODO
+break;
+case 'DELETE':
+// Placeholder for deleting a TODO
+break;
 }
-
 ?>
