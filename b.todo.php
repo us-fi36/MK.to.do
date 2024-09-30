d02003f2ec5e3d42458bb5033df5d716db26ef3b
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-30 14:55:26 +0200

    Update b.todo.php

--- b.todo.php
+++ b.todo.php
@@ -20,7 +20,7 @@ $todo_items = json_decode(file_get_contents($todo_file), true);
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Rückgabe der TODOs
     echo json_encode($todo_items);
- 
+}
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
@@ -30,10 +30,19 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         write_log("READ", $todo_items);
         break;
 
-    case "POST":
-        // Add Todo (CREATE)
-        write_log("CREATE", null);
-        break;
+        case "POST":
+            // Get data from the input stream.
+            $data = json_decode(file_get_contents('php://input'), true);
+            // Create new todo item.
+            $new_todo = ["id" => uniqid(), "title" => $data['title']];
+            // Add new item to our todo item list.
+            $todo_items[] = $new_todo;
+            // Write todo items to JSON file.
+            file_put_contents($todo_file, json_encode($todo_items));
+            // Return the new item.
+            echo json_encode($new_todo);
+            write_log("CREATE", $new_todo);
+            break;
     case "PUT":
         // Change Todo (UPDATE)
         write_log("PUT", null);
