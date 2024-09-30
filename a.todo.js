5722b5e78dba0daf1c8bccaeaf83e9c695b9a5ca
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-30 13:36:34 +0200

    Update and rename a.script.js to a.todo.js

---
+++ a.todo.js
@@ -0,0 +1,36 @@
+// put your script elements here:
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Corrected the URL.
+    const apiUrl = "http://172.30.148.126/Todolist/todo.php";
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const todoList = document.getElementById('todo-list');
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item.title;
+            todoList.appendChild(li);
+        });
+    });
+    document.getElementById('todo-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const todoInput = document.getElementById('todo-input').value;
+        fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: todoInput })
+        })
+        .then(response => response.json())
+        .then(data => {
+        const todoList = document.getElementById('todo-list');
+        const li = document.createElement('li');
+        li.textContent = data.title;
+        todoList.appendChild(li);
+        });
+    });
+});
