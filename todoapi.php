f18bc8ae5734aa4e771530d54e8c820138d6ef2e
Mazemk2 <159236596+Mazemk2@users.noreply.github.com>
2024-09-30 10:14:33 +0200

    Update todoapi.php
    add Log function to todo.php

--- todoapi.php
+++ todoapi.php
@@ -13,5 +13,14 @@ break;
 case 'DELETE':
 // Placeholder for deleting a TODO
 break;
+
+// LOG function in PHP
+function write_log($action, $data) {
+$log = fopen('log.txt', 'a');
+$timestamp = date('Y-m-d H:i:s');
+fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+fclose($log);
+  
+}
 }
 ?>
